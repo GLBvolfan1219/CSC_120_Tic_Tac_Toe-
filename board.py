685e2e0b10cd1612fff6f3d9ba26f8151816ca60
GLBvolfan1219 <103973461+GLBvolfan1219@users.noreply.github.com>
2022-04-19 15:36:16 -0400

    Update board.py

--- board.py
+++ board.py
@@ -10,5 +10,22 @@ class TicTacToe:
         for i in range(3):
             row = []
             for j in range(3):
-                row.append('-')
+                row.append('"')
             self.board.append(row)
+
+    def show_board(self):
+        for row in self.board:
+            for item in row:
+                print(item, end=" ")
+            print()
+
+    def start(self):
+        self.create_board()
+
+        # showing view of board
+        print()
+        self.show_board()
+
+# starting the game
+tic_tac_toe = TicTacToe()
+tic_tac_toe.start()
